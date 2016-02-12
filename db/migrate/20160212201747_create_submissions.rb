655ef69cdbc6186a04ae8b47fd0d9f6408205683
Daniel <dla107@sfu.ca>
2016-02-12 20:33:24 +0000

    add columns to models

--- db/migrate/20160212201747_create_submissions.rb
+++ db/migrate/20160212201747_create_submissions.rb
@@ -1,7 +1,9 @@
 class CreateSubmissions < ActiveRecord::Migration
   def change
     create_table :submissions do |t|
-
+      t.string :link
+      t.string :description
+      t.integer :price
       t.timestamps null: false
     end
   end
--- db/migrate/20160212201810_create_item_displays.rb
+++ db/migrate/20160212201810_create_item_displays.rb
@@ -1,7 +1,8 @@
 class CreateItemDisplays < ActiveRecord::Migration
   def change
     create_table :item_displays do |t|
-
+      t.string :description
+      t.decimal :price
       t.timestamps null: false
     end
   end
