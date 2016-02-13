8d7b301cea7761b9acc87b49d65f1f517e7fb446
Jordan Siaw <jordansiaw.cy@gmail.com>
2016-02-13 00:48:13 +0000

    Add form and index views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+   root 'submissions#index'
+   resources :submissions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
